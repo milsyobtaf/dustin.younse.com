e31420f11a54e2544f9694be1c0436d033f4df88
Dustin Younse <milsyobtaf@gmail.com>
2016-03-20 13:08:46 -0400

    Adjusted rsync excludes

--- gruntfile.js
+++ gruntfile.js
@@ -121,7 +121,7 @@ module.exports = function (grunt) {
           rsync: {
             options: {
               args: ["--verbose"],
-              exclude: [".git*","*.scss","sass","node_modules","_inspiration","package.json",".*.map","*.patch"],
+              exclude: [".git*","*.scss","sass",".sass-cache","node_modules","_inspiration","package.json","*.map","*.patch","gruntfile.js"],
               recursive: true
             },
             prod: {
