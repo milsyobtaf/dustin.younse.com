807c025ddefb7a1848590df47a9c91883adf3030
Dustin Younse <milsyobtaf@gmail.com>
2025-08-19 23:56:12 -0400

    Update config to use standard object name

--- .eleventy.js
+++ .eleventy.js
@@ -1,30 +1,30 @@
-export default function (config) {
-  config.addPassthroughCopy({ public: "./" })
-  config.addShortcode("year", () => `${new Date().getFullYear()}`)
-  config.setBrowserSyncConfig({
+export default function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ public: "./" })
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`)
+  eleventyConfig.setBrowserSyncConfig({
     files: ["dist/**/*"],
     open: true,
   })
-  config.addCollection("resumeEducation", function (collection) {
+  eleventyConfig.addCollection("resumeEducation", function (collection) {
     return collection.getFilteredByTags("resume", "education")
   })
-  config.addCollection("resumeRecentExperience", function (collection) {
+  eleventyConfig.addCollection("resumeRecentExperience", function (collection) {
     return collection
       .getFilteredByGlob("_data/_resume/experience/recent/*.md")
       .reverse()
   })
-  config.addCollection("resumePriorExperience", function (collection) {
+  eleventyConfig.addCollection("resumePriorExperience", function (collection) {
     return collection
       .getFilteredByGlob("_data/_resume/experience/prior/*.md")
       .reverse()
   })
-  config.addCollection("resumeSkills", function (collection) {
+  eleventyConfig.addCollection("resumeSkills", function (collection) {
     return collection.getFilteredByTags("resume", "skills")
   })
-  config.addFilter("limit", function (arr, limit) {
+  eleventyConfig.addFilter("limit", function (arr, limit) {
     return arr.slice(0, limit)
   })
-  config.addFilter("sortByOrder", function (values) {
+  eleventyConfig.addFilter("sortByOrder", function (values) {
     let vals = [...values]
     return vals.sort((a, b) => Math.sign(a.data.order - b.data.order))
   })
