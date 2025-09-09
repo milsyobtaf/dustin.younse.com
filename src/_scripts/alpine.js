0c0a804f6273f6df76c82248bc8c164c00f33a27
Dustin Younse <milsyobtaf@gmail.com>
2025-09-09 16:32:45 -0400

    Add some nice animationerinos

--- src/_scripts/alpine.js
+++ src/_scripts/alpine.js
@@ -1,18 +1,27 @@
-import Alpine from 'alpinejs'
+import Alpine from "alpinejs"
+import collapse from "@alpinejs/collapse"
+
+Alpine.plugin(collapse)
 
 window.Alpine = Alpine
 
 // Start Alpine when the page is ready.
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   Alpine.start()
-});
+})
 
 // Basic Store Example in Alpine.
-window.addEventListener('alpine:initializing', () => {
-  Alpine.store('nav', {
+window.addEventListener("alpine:initializing", () => {
+  Alpine.store("nav", {
     isOpen: false,
-    close() { return this.isOpen = false },
-    open() { return this.isOpen = true },
-    toggle() { return this.isOpen = !this.isOpen }
+    close() {
+      return (this.isOpen = false)
+    },
+    open() {
+      return (this.isOpen = true)
+    },
+    toggle() {
+      return (this.isOpen = !this.isOpen)
+    },
   })
-});
+})
