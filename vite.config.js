81d6ee4a0940d88897c39ed6a35c9a655820e697
Dustin Younse <milsyobtaf@gmail.com>
2026-03-16 23:22:16 -0400

    Update packages and configs

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,17 @@
-import tailwindcss from "@tailwindcss/vite"
-
 /** @type {import('vite').UserConfig} */
 export default {
-  plugins: [tailwindcss()],
-  publicDir: "public",
+  publicDir: 'public',
   clearScreen: false,
   build: {
-    outDir: "./dist/assets",
-    assetsDir: "",
+    outDir: './dist/assets',
+    assetsDir: '',
     rollupOptions: {
-      input: ["./src/_scripts/_main.js", "./src/_styles/_main.css"],
+      input: ['./src/_scripts/_main.js', './src/_styles/_main.css'],
       output: {
         entryFileNames: `[name].js`,
         chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`,
-      },
-    },
-  },
-}
+        assetFileNames: `[name].[ext]`
+      }
+    }
+  }
+};
